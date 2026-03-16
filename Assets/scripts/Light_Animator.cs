2a439b5031195fdfc3edfb3d4112230ef63b5c78
Calla <Calla.Aitken@gmail.com>
2026-03-16 12:10:02 +0000

    buttons

--- Assets/scripts/Light_Animator.cs
+++ Assets/scripts/Light_Animator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 using Random = UnityEngine.Random;
 
 [System.Serializable]
@@ -11,70 +12,79 @@ public class LightAnimator : MonoBehaviour
     public float lightTime = 3;
 
     int currentLight;
+    int currentButton;
+
+    public bool buttonPress;
 
     public Light[] lights;
+    public GameObject[] buttons;
     int[] sequence = new int[8];
     public int counter = 0;
 
     public void Begin()
     {
-        
         Debug.Log("Help");
         StartCoroutine("ChangeLight");
-        
     }
+
+    public void GetButtonPress()
+    {
+        Debug.Log("L");
+        buttonPress = true;
+    }
+
 
     IEnumerator ChangeLight()
     {
         // change to the next random light
         int randomIndex = Random.Range(0, 4);
         currentLight = randomIndex;
-        Debug.Log("the index is is" +randomIndex);
-        Debug.Log("the curent is is" +currentLight);
-        
-        // disable the current light
-       
         lights[currentLight].intensity = 50;
 
-        //start again if 8 lights have not flashed
         
+        // disable the current light
         yield return new WaitForSeconds(lightTime);
         lights[currentLight].intensity = 0;
         yield return new WaitForSeconds(lightTime);
 
-        
-        
-        
+        //start again if 8 lights have not flashed
         counter++;
-        Debug.Log("this is counter" + counter);
         
-            
-       
         if (counter <8)
-        {
-            
+        { 
+            //Adds the current light's number to an array
             sequence[counter] = currentLight;
             StartCoroutine("ChangeLight");
-       
         }
         else
         {
+            //Print full sequence to the console 
             lights[currentLight].intensity = 0;
             String sequenceFull = "";
-            String sequenceToString = "";
             lights[currentLight].intensity = 0;
             for (int i = 0; i < 8; i++)
             {
-                sequenceFull = sequenceFull + sequence[i] +","; 
+                sequenceFull =  sequenceFull + sequence[i] +",";
                 
             }
-            Debug.Log(sequenceFull);
-            
-
+            Debug.Log("The light sequence is " + sequenceFull);
+            StartCoroutine("ButtonInputs");
         }
 
     }
-    public void Addtoarray(int currentLight){
-        
+
+    IEnumerator ButtonInputs()
+    {
+        String sequenceFull = "";
+        for (int i = 0; i < 8; i++)
+        {
+            currentButton = sequence[i];
+            sequenceFull =  sequenceFull + sequence[i] + ",";
+            yield return new WaitUntil(() => buttonPress = true);
+        }
+        Debug.Log("The button sequence is " + sequenceFull);
     }
+
+
+   
 }
